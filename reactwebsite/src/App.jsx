87d8edc514584bd0f9fe426a1175c92992908d38
protyoy kumar ghosh <pghosh0615@gmail.com>
2023-02-05 11:19:31 +0530

    first

---
+++ reactwebsite/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Services from "./Services";
+
+const App = () => {
+  return (
+    <>
+      <Home></Home>
+      <About></About>
+      <Contact></Contact>
+      <Services></Services>
+    </>
+  )
+};
+
+export default App;
